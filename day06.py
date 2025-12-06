6e53b0f1d43b0ff7c70dc7319aabadd00d210e83
Alasdair Nicol <alasdair@thenicols.net>
2025-12-06 16:57:09 +0000

    Refine day 6

--- day06.py
+++ day06.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
-import operator
-from typing import Callable, Any
 import functools
+import itertools
+import operator
+from typing import Any, Callable, Iterable
 
 operators = {
     "+": operator.add,
@@ -9,15 +10,17 @@ operators = {
 }
 
 
-def digits_to_num(digits):
+def digits_to_num(digits: list[int]) -> int:
     return functools.reduce(lambda x, y: 10 * x + y, digits, 0)
 
 
-def calc_column(op, column):
+def calc_column(op: Callable[[Any, Any], Any], column: Iterable[int]):
     return functools.reduce(op, column)
 
 
-def calc_columns(ops, columns):
+def calc_columns(
+    ops: list[Callable[[Any, Any], Any]], columns: Iterable[Iterable[int]]
+):
     return sum(calc_column(op, column) for op, column in zip(ops, columns))
 
 
@@ -28,18 +31,15 @@ def do_part_1(row_strings: list[str], ops: list[Callable[[Any, Any], Any]]) -> i
 
 
 def do_part_2(row_strings: list[str], ops: list[Callable[[Any, Any], Any]]) -> int:
-    new_columns = list(
-        digits_to_num([int(x) for x in l if x.strip()]) for l in zip(*row_strings)
+    columns = list(
+        digits_to_num([int(c) for c in col if c.strip()]) for col in zip(*row_strings)
     )
-    batched_columns = []
-    batch = []
-    for column in new_columns:
-        if column == 0:
-            batched_columns.append(batch)
-            batch = []
-        else:
-            batch.append(column)
-    return calc_columns(ops, batched_columns)
+    grouped_columns = [
+        list(group)
+        for value, group in itertools.groupby(columns, key=lambda x: x != 0)
+        if value  # keep only the non-zero groups
+    ]
+    return calc_columns(ops, grouped_columns)
 
 
 def main() -> None:
