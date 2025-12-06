6f15d49ac406ceedba4fe1e7e61801f90c6da778
Alasdair Nicol <alasdair@thenicols.net>
2025-12-06 17:15:22 +0000

    Day 6 switch to strategy pattern

--- day06.py
+++ day06.py
@@ -24,13 +24,18 @@ def calc_columns(
     return sum(calc_column(op, column) for op, column in zip(ops, columns))
 
 
-def do_part_1(row_strings: list[str], ops: list[Callable[[Any, Any], Any]]) -> int:
+def extract_columns_part_1(row_strings: list[str]) -> Iterable[Iterable[int]]:
+    """
+    Split rows on whitespace then transpose to get columns
+    """
     rows = [[int(x) for x in row.split()] for row in row_strings]
-    columns = zip(*rows)
-    return calc_columns(ops, columns)
+    return zip(*rows)
 
 
-def do_part_2(row_strings: list[str], ops: list[Callable[[Any, Any], Any]]) -> int:
+def extract_columns_part_2(row_strings: list[str]) -> Iterable[Iterable[int]]:
+    """
+    Concat digits vertically. Groups are separated by a column of whitespace.
+    """
     columns = list(
         digits_to_num([int(c) for c in col if c.strip()]) for col in zip(*row_strings)
     )
@@ -39,7 +44,16 @@ def do_part_2(row_strings: list[str], ops: list[Callable[[Any, Any], Any]]) -> i
         for value, group in itertools.groupby(columns, key=lambda x: x != 0)
         if value  # keep only the non-zero groups
     ]
-    return calc_columns(ops, grouped_columns)
+    return grouped_columns
+
+
+def solve(
+    row_strings: list[str],
+    ops: list[Callable[[Any, Any], Any]],
+    extract_columns_function: Callable[[list[str]], Iterable[Iterable[int]]],
+):
+    columns = extract_columns_function(row_strings)
+    return calc_columns(ops, columns)
 
 
 def main() -> None:
@@ -47,10 +61,10 @@ def main() -> None:
     *row_strings, operators_string = lines
     ops = [operators[o] for o in operators_string.split()]
 
-    part_1 = do_part_1(row_strings, ops)
+    part_1 = solve(row_strings, ops, extract_columns_part_1)
     print(f"{part_1=}")
 
-    part_2 = do_part_2(row_strings, ops)
+    part_2 = solve(row_strings, ops, extract_columns_part_2)
     print(f"{part_2=}")
 
 
